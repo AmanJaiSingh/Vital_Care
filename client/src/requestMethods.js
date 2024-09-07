40e3add1ee26b068461bbf0b78b11349c06666af
AmanJaiSingh <76746180+AmanJaiSingh@users.noreply.github.com>
2024-09-08 00:55:19 +0530

    lelo

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+// const dotenv = require("dotenv");
 
-const BASE_URL = process.env.BACK_API;
+const BASE_URL = `http://ec2-65-0-106-88.ap-south-1.compute.amazonaws.com:5000/api/`;
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTY5YWE3ZWJiNWRmYmI3YTgxMmQzNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzM3MzM5OSwiZXhwIjoxNjQzNjMyNTk5fQ._Hhwlf-8kGZoXxtbMKY2opVxTt6brHCCCnBiCcoOC8k";
 
