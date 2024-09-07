22b2b51788fa77dbec955b8c949480b60bcbf183
AmanJaiSingh <76746180+AmanJaiSingh@users.noreply.github.com>
2024-09-07 23:57:09 +0530

    api changed

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = process.env.BACK_API;
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTY5YWE3ZWJiNWRmYmI3YTgxMmQzNyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzM3MzM5OSwiZXhwIjoxNjQzNjMyNTk5fQ._Hhwlf-8kGZoXxtbMKY2opVxTt6brHCCCnBiCcoOC8k";
 
